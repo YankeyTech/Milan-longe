244da94e81441ef8fada757cd1683c7bb6c96ac9
Godwin Yankey <36565894+YankeyTech@users.noreply.github.com>
2026-05-09 00:29:55 +0000

    main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return (
+    <div style={{ background: "black", color: "white", height: "100vh" }}>
+      <h1>MILAN Lounge & Bar</h1>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
