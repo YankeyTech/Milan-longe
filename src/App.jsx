9b83a34d6219e3ca2f0a78e40bdb2a00f11b60b1
Godwin Yankey <36565894+YankeyTech@users.noreply.github.com>
2026-05-09 01:06:48 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,5 @@
 export default function App() {
-  return (
-    <div style={{
-      height: "100vh",
-      background: "black",
-      color: "white",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      fontSize: "40px",
-      fontWeight: "bold"
-    }}>
-      MILAN LOUNGE & BAR IS LIVE 🚀
-    </div>
-  );
+  return <h1 style={{ color: "white", background: "black" }}>
+    MILAN WORKING 🚀
+  </h1>;
 }
